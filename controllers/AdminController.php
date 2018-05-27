27a87a4b6bd4a78a0a9bfa4e165c46e745b9032b
Juan Pedro Postigo <jppostigo97@gmail.com>
2018-05-27 17:35:52 +0200

    Controlador panel de administración

---
+++ controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+	class AdminController {
+
+		public function __construct() {
+			Application::$subtitle = "adminPanel";
+		}
+
+		public function index() {
+			$userid = (isset($_SESSION["id"]))? $_SESSION["id"] : 0;
+			$user   = Connection::getConnection()->query("SELECT * FROM user WHERE id=$userid;");
+			if ($user->num_rows) {
+				View::load("admin-panel");
+			} else {
+				header("Location: ..");
+				exit;
+			}
+		}
+	}
+?>
