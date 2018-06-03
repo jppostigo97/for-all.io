cfecb07aa30457b900afcb2a57e0fe10287254a6
Juan Pedro Postigo <jppostigo97@gmail.com>
2018-06-03 18:43:13 +0200

    Panel de administración: crear foro (en foros)

--- views/dynamic/adm-forums.php
+++ views/dynamic/adm-forums.php
@@ -1,5 +1,7 @@
 <div id="admin-forums">
 	<h2>Foros y subforos</h2>
+
+	<modal-opener text="Nuevo foro" target="create-forum"></modal-opener>
 	
 	<div class="forum" ng-repeat="forum in $ctrl.forums">
 		<h3 class="forum-title">{{ forum.title }}</h3>
@@ -11,4 +13,37 @@
 			</li>
 		</ul>
 	</div>
+</div>
+
+<div id="create-forum" class="modal">
+
+	<div class="modal-container">
+		<div class="modal-header">
+			<span class="modal-title">Nuevo foro</span>
+			<span class="close">&times;</span>
+		</div>
+
+		<div class="modal-content">
+			
+			<form action="api/create_forum" method="POST">
+
+				<div class="form-field">
+					<label for="newForumTitle">Título</label>
+					<input type="text" name="title" />
+				</div>
+
+				<div class="form-field">
+					<label for="newForumDescription">Descripción</label>
+					<input type="text" name="description" />
+				</div>
+
+				<div>
+					<button type="submit">Crear</button>
+				</div>
+
+			</form>
+
+		</div>
+	</div>
+
 </div>
