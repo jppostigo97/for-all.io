8abbabe6067d95531af3480454ca6a3a5891b6b7
Juan Pedro Postigo <jppostigo97@gmail.com>
2018-06-04 18:33:19 +0200

    Se modifica el filtro de fechas de AngularJS

--- app/filters/date.filter.js
+++ app/filters/date.filter.js
@@ -4,8 +4,20 @@ angular.module("forAll")
 		return function(input) {
 
 			let output = input.split(" ")[0];
-			output = output.split("-").reverse().join("-");
+			output = output.split("-").reverse().join("/");
 			return output;
+
+		};
+
+	})
+	.filter("timefilter", function($filter) {
+
+		return function(input) {
+
+			let output = input.split(" ")[1];
+			output = output.split(":");
+			output.pop();
+			return output.join(":");
 
 		};
 
