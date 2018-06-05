83664863033f1d886d70002a9a16043263c53c4f
Juan Pedro Postigo <jppostigo97@gmail.com>
2018-06-05 22:02:15 +0200

    Panel de administración: finalizado - Se termina el desarrollo del panel de administración - Se implementa la creación, edición y eliminación de foros y subforos

--- app/components/forums.adminPanel.js
+++ app/components/forums.adminPanel.js
@@ -4,6 +4,10 @@ angular.module("forAll.admin")
 		controller: function($http) {
 			this.forums = [];
 
+			this.params = {};
+
+			this.newSubforumParent = null;
+
 			this.getForums = () => {
 				$http.get("api/forums").then((response) => {
 					let forumList = response.data;
@@ -20,6 +24,119 @@ angular.module("forAll.admin")
 				}, (error) => {
 					console.log("Error al recuperar los foros");
 				});
+			};
+
+			this.editForum = (forumId) => {
+				if (!forumId) {
+					return {
+						save: () => {
+							let p = this.params.editForum;
+							if (typeof p.id != "undefined" && typeof p.title != "undefined" &&
+								typeof p.description != "undefined" && p.id != 0 &&
+								p.title !== "" && p.description != "") {
+								let f = document.querySelector("#edit-forum form");
+
+								f.action =
+									"api/edit_forum/" + p.id;
+								f.submit();
+							} else {
+								alert("No puedes dejar ningún campo vacío.");
+							}
+						}
+					};
+				}
+				
+				this.params.editForum = {};
+				
+				for (let forum of this.forums) {
+					if (forum.id == forumId) {
+						this.params.editForum =  {
+							id: forum.id,
+							title: forum.title,
+							description: forum.description
+						};
+						break;
+					}
+				}
+			};
+
+			this.deleteForum = (forum) => {
+				if (confirm("¿Eliminar el foro " + forum.title + " junto con sus subforos?")) {
+					$http.get("api/delete_forum/" + forum.id).then((response) => {
+						if (response.data.deleted && response.data.deleted == "true") {
+							for (let i in this.forums) {
+								if (this.forums[i].id == forum.id) this.forums.splice(i, 1);
+							}
+						}
+					}, (error) => {
+						alert("No se ha podido eliminar el foro.");
+					});
+				}
+			};
+
+			this.saveSubforum = {
+				new: () => {
+					if (this.newSubforumParent != null) {
+						let f = document.querySelector("#create-subforum form");
+
+						f.action = "api/create_subforum/" + this.newSubforumParent;
+						f.submit();
+					} else {
+						alert("No ha sido posible crear un nuevo subforo.");
+					}
+				},
+				edit: () => {
+					let p = this.params.editSubforum;
+					if (typeof p.id != "undefined" && typeof p.title != "undefined" &&
+						typeof p.description != "undefined" && p.id != 0 &&
+						p.title !== "" && p.description != "") {
+						let f = document.querySelector("#edit-subforum form");
+
+						f.action = "api/edit_subforum/" + p.id;
+						f.submit();
+					} else {
+						alert("No puedes dejar ningún campo vacío.");
+					}
+				}
+			};
+
+			this.editSubforum = (forumId, subforumId) => {
+				this.params.editSubforum = {};
+				
+				for (let forum of this.forums) {
+					if (forum.id == forumId) {
+						for (let sub of forum.subforums) {
+							if (sub.id == subforumId) {
+								this.params.editSubforum =  {
+									id: sub.id,
+									title: sub.title,
+									description: sub.description
+								};
+								break;
+							}
+						}
+						break;
+					}
+				}
+			};
+
+			this.deleteSubforum = (forumId, subforum) => {
+				if (confirm("¿Eliminar el subforo " + subforum.title + "?")) {
+					$http.get("api/delete_subforum/" + subforum.id).then((response) => {
+						if (response.data.deleted && response.data.deleted == "true") {
+							for (let i in this.forums) {
+								if (this.forums[i].id == forumId) {
+									for (let j in this.forums[i].subforums) {
+										if (this.forums[i].subforums[j].id == subforum.id)
+											this.forums[i].subforums.splice(j, 1);
+									}
+								}
+							}
+						}
+					}, (error) => {
+						alert("No se ha podido eliminar el subforo.");
+					});
+				}
 			};
 
 			this.getForums();
--- views/dynamic/adm-forums.php
+++ views/dynamic/adm-forums.php
@@ -1,19 +1,54 @@
 <div id="admin-forums">
 	<h2>Foros y subforos</h2>
 
-	<modal-opener text="Nuevo foro" target="create-forum"></modal-opener>
+	<modal-opener target="create-forum"><i class="fa fa-plus"></i></modal-opener>
 	
-	<div class="forum" ng-repeat="forum in $ctrl.forums">
-		<h3 class="forum-title">{{ forum.title }}</h3>
+	<div class="forum" ng-repeat="forum in $ctrl.forums track by $index">
+
+		<h3 class="forum-title">
+			{{ forum.title }}
+		</h3>
+		
 		<p class="forum-description">{{ forum.description }}</p>
+		
+		<div class="action-buttons">
+			<modal-opener ng-click="$ctrl.newSubforumParent = forum.id" target="create-subforum">
+				<i class="fa fa-plus"></i>
+			</modal-opener>
+
+			<modal-opener ng-click="$ctrl.editForum(forum.id)" target="edit-forum">
+				<i class="fa fa-pen-square"></i>
+			</modal-opener>
+
+			<button ng-click="$ctrl.deleteForum(forum)">
+				<i class="fa fa-trash"></i>
+			</button>
+		</div>
+
 		<ul class="forum-subforums">
 			<li class="subforum" ng-repeat="subforum in forum.subforums">
-				<a href="forum/show/{{ subforum.id }}">{{ subforum.title }}	</a>
+
+				<a href="forum/show/{{ subforum.id }}">{{ subforum.title }}</a>
+				
 				<p>{{ subforum.description }}</p>
+
+				<div class="action-buttons">
+					<modal-opener ng-click="$ctrl.editSubforum(forum.id, subforum.id)" target="edit-subforum">
+						<i class="fa fa-pen-square"></i>
+					</modal-opener>
+
+					<button ng-click="$ctrl.deleteSubforum(forum.id, subforum)">
+						<i class="fa fa-trash"></i>
+					</button>
+				</div>
+
 			</li>
 		</ul>
+
 	</div>
 </div>
+
+<!-- Modals -->
 
 <div id="create-forum" class="modal">
 
@@ -28,12 +63,12 @@
 			<form action="api/create_forum" method="POST">
 
 				<div class="form-field">
-					<label for="newForumTitle">Título</label>
+					<label for="title">Título</label>
 					<input type="text" name="title" />
 				</div>
 
 				<div class="form-field">
-					<label for="newForumDescription">Descripción</label>
+					<label for="description">Descripción</label>
 					<input type="text" name="description" />
 				</div>
 
@@ -46,4 +81,114 @@
 		</div>
 	</div>
 
+</div>
+
+
+
+<div id="edit-forum" class="modal">
+
+	<div class="modal-container">
+		<div class="modal-header">
+			<span class="modal-title">Editar foro</span>
+			<span class="close">&times;</span>
+		</div>
+
+		<div class="modal-content">
+
+			<form ng-submit="$ctrl.editForum().save()" method="POST">
+
+				<div class="form-field">
+					<label for="title">Título</label>
+					<input type="text" name="title" ng-model="$ctrl.params.editForum.title" />
+				</div>
+
+				<div class="form-field">
+					<label for="description">Descripción</label>
+					<input type="text" name="description"
+						ng-model="$ctrl.params.editForum.description" />
+				</div>
+
+				<div>
+					<button type="submit">Guardar</button>
+				</div>
+
+			</form>
+			
+		</div>
+	</div>
+
+</div>
+
+
+<div id="create-subforum" class="modal">
+	<div class="modal-container">
+
+		<div class="modal-header">
+			<span class="modal-title">
+				Nuevo subforo
+			</span>
+			
+			<span class="close">&times;</span>
+		</div>
+
+		<div class="modal-content">
+		
+			<form ng-submit="$ctrl.saveSubforum.new()" method="POST">
+			
+				<div class="form-field">
+					<label for="title">Título</label>
+					<input type="text" name="title" />
+				</div>
+
+				<div class="form-field">
+					<label for="description">Descripción</label>
+					<input type="text" name="description" />
+				</div>
+
+				<div>
+					<button type="submit">Crear</button>
+				</div>
+
+			</form>
+
+		</div>
+
+	</div>
+</div>
+
+<div id="edit-subforum" class="modal">
+	<div class="modal-container">
+
+		<div class="modal-header">
+			<span class="modal-title">
+				Editar subforo
+			</span>
+			
+			<span class="close">&times;</span>
+		</div>
+
+		<div class="modal-content">
+
+			<form ng-submit="$ctrl.saveSubforum.edit()" method="POST">
+			
+				<div class="form-field">
+					<label for="title">Título</label>
+					<input type="text" name="title" ng-model="$ctrl.params.editSubforum.title" />
+				</div>
+
+				<div class="form-field">
+					<label for="description">Descripción</label>
+					<input type="text" name="description"
+						ng-model="$ctrl.params.editSubforum.description" />
+				</div>
+
+				<div>
+					<button type="submit">Guardar</button>
+				</div>
+
+			</form>
+
+		</div>
+
+	</div>
 </div>
